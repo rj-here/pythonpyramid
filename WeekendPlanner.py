e5327d5757dd1afe66fafee5a3fdff436ed64d5f
rj-here <r1sh1j@my.yorku.ca>
2025-06-25 17:33:39 -0400

    Create WeekendPlanner.py

---
+++ WeekendPlanner.py
@@ -0,0 +1,9 @@
+#This program will be to help you plan a weekend. This would incorporate various categories, in try to give you a brief overview of your plans
+print('Hello there! This will be a weekend planner. This is going to be more focused on a general across tasks I have or observe people having on the weekend.')
+
+def whatHouseTasks():
+    print('What house related tasks do you need to complete? Seperate them with |')
+    houseTasks = input
+    houseTasksList = houseTasks.split('|')
+
+whatHouseTasks()

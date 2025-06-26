92aef5b38914c3cd3b9fa57187df767da0705491
rj-here <r1sh1j@my.yorku.ca>
2025-06-26 11:08:05 -0400

    Updates
    Setting up the different types. Yet to learn inputs, so commenting those parts for now.

--- WeekendPlanner.py
+++ WeekendPlanner.py
@@ -3,7 +3,21 @@ print('Hello there! This will be a weekend planner. This is going to be more foc
 
 def whatHouseTasks():
     print('What house related tasks do you need to complete? Seperate them with |')
-    houseTasks = input
-    houseTasksList = houseTasks.split('|')
+    #houseTasks = input
+    #houseTasksList = houseTasks.split('|')
 
 whatHouseTasks()
+
+def whatOccasionalTasks():
+    print('What occasional tasks (getting a haircut, cutting the grass, etc) do you need to complete? Seperate with them |')
+    #occasionalTasks = input
+    #occasionalTasksList = occasionalTasks.split('|')
+
+whatOccasionalTasks()
+
+def whatSpecialThings():
+    print("What special things (go out for dinner, watch a movie, etc) would you like to do? Seperate them with |")
+    #specialThings = input
+    #specialThingsList = specialThings.split('|')
+
+whatSpecialThings()

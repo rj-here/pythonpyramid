9080f1cbe61168708a42e7d951570d366b3ce59d
rj-here <r1sh1j@my.yorku.ca>
2025-07-03 12:08:32 -0400

    Progress

--- TripPlanner.py
+++ TripPlanner.py
@@ -2,6 +2,20 @@
 print('Welcome! This is going to be a trip planner, assisting in details like the location, activities, people accompanying, shopping list, and more.')
 def typeOfTrip():
     print('What type of trip is being planned? (Day trip, weekend trip, week-long, or just a good old long proper vacation?)')
-    
+
+def tripLocation():
+    print('Where is the trip going to be?')
+
+def tripActivities():
+    print('What are the objectives of yours for the trip?')
+
+def tripCompany():
+    print('Who is joining on you on the trip?')
+
+def tripShoppingList():
+    print('What do you plan to get while on the trip? I try and get something that makes me remember the trip, whether a magnet or a t-shirt')
+
+def tripBudget():
+    print('What is trip\'s budget?')
 #© 2025 Rishi
-#Last updated: 2nd July, 2025
+#Last updated: 3rd July, 2025

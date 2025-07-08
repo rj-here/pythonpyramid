d2a145f1b8ef4c1dd8667fd4a7ece1458393800e
rj-here <r1sh1j@my.yorku.ca>
2025-07-08 09:00:08 -0400

    Improving them

--- TripPlanner.py
+++ TripPlanner.py
@@ -1,21 +1,47 @@
 #This program is supposed to help you plan a trip. This will feature many parts, and is partly inspired by my personal trip planning template.
+#This is just a quick and basic way for you to note down your ideas, which you can then use to plan out further. I currently do not plan to expand this, but I may consider in the future.
+#This is part of a growing collection of Python programs, for me to get a proper grasp of basics, and for you to see.
 print('Welcome! This is going to be a trip planner, assisting in details like the location, activities, people accompanying, shopping list, and more.')
 def typeOfTrip():
-    print('What type of trip is being planned? (Day trip, weekend trip, week-long, or just a good old long proper vacation?)')
+    tripType = input('What type of trip is being planned? (Day trip, weekend trip, week-long, or just a good old long proper vacation?)\n')
+    print(f'This trip is a {tripType}.')
+
+typeOfTrip()
 
 def tripLocation():
-    print('Where is the trip going to be?')
+    location = input('Where is the trip going to be?\n')
+    print(f'The trip is at {location}.')
+
+tripLocation()
 
 def tripActivities():
-    print('What are the objectives of yours for the trip?')
+    activities = input('What are the objectives of yours for the trip? Seperate them with | \n')
+    print('The activities planned for the trip are:')
+    for activity in activities.split(' | '):
+        print({activity})
+
+tripActivities()   
 
 def tripCompany():
     print('Who is joining on you on the trip?')
+    people = input('Input the people joining you, separated by | \n')
+    peopleList = people.split(' | ')
+    print(f'All these people are joining you on the trip: {peopleList}')
+
+tripCompany()
 
 def tripShoppingList():
-    print('What do you plan to get while on the trip? I try and get something that makes me remember the trip, whether a magnet or a t-shirt')
+    print('What do you plan to get while on the trip? I try and get something that makes me remember the trip, whether a magnet or a t-shirt \n')
+    shopping = input('Input the items you plan to get, separated by | \n')
+    shoppingList = shopping.split(' | ')
+    print(f'You\'re planning to get: {shoppingList} during the trip')
+
+tripShoppingList()
 
 def tripBudget():
-    print('What is trip\'s budget?')
+    budget = input('What\'s the rough budget for the trip? \n')
+    print(f'The budget for the trip is: {budget}')
+tripBudget()
+
 #© 2025 Rishi
-#Last updated: 3rd July, 2025
+#Last updated: 8th July, 2025
--- WeekendPlanner.py
+++ WeekendPlanner.py
@@ -1,44 +1,46 @@
 #This program will be to help you plan a weekend. This would incorporate various categories, in try to give you a brief overview of your plans
+#This is a basic program to help you list out your thoughts, and can use this guide your plans. I currently don't intend to expand this, but I will not rule out future considerations.
+#This is part of a growing collection of Python programs, for me to get a proper grasp of basics, and for you to see.
 print('Hello there! This will be a weekend planner. This is going to be more focused on a general across tasks I have or observe people having on the weekend.')
 
 def whatHouseTasks():
     print('What house related tasks do you need to complete? Seperate them with |')
-    houseTasks = 'Groceries | Laundry'
+    houseTasks = input('House tasks: ') #inputting the house tasks
     houseTasksList = houseTasks.split(' | ')
     print('House tasks: ')
     print('One-by-one:')
     for task in houseTasksList: #printing each one by one
         print(task)
-    print('All at once:') #printing them all, seeing how printing a list goes
+    print('So, your house tasks for the weekend are: ') #printing them all, seeing how printing a list goes
     print(houseTasksList)
 
 whatHouseTasks()
 
 def whatOccasionalTasks():
     print('What occasional tasks (getting a haircut, cutting the grass, etc) do you need to complete? Seperate with them |')
-    occasionalTasks = 'Get a haircut | Cut the grass'
+    occasionalTasks = input('Occasional tasks: ') #inputting the occasional tasks
     occasionalTasksList = occasionalTasks.split(' | ')
     print('Occasional tasks: ')
     print('One-by-one:')
     for task in occasionalTasksList: #printing each one by one
         print(task)
-    print('All at once:') #printing them all, seeing how printing a list goes
+    print('So, your occasional tasks for the weekend are: ') #printing them all, seeing how printing a list goes
     print(occasionalTasksList)
 
 whatOccasionalTasks()
 
 def whatSpecialThings():
     print("What special things (go out for dinner, watch a movie, etc) would you like to do? Seperate them with |")
-    specialThings = 'Going out for dinner | Meeting friends'
+    specialThings = input('Special things: ') #inputting the special things
     specialThingsList = specialThings.split(' | ')
     print('Special things: ')
     print('One-by-one:')
     for task in specialThingsList: #printing each one by one
         print(task)
-    print('All at once:') #printing them all, seeing how printing a list goes
+    print('So, the special things, hopefully making your weekend a little more special are: ') #printing them all, seeing how printing a list goes
     print(specialThingsList)
 
 whatSpecialThings()
 
 #© 2025 Rishi
-#Last updated: 2nd July, 2025
+#Last updated: 8th July, 2025

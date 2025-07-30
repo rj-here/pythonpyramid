99fa9f855f4f8757f18a19e49def41d090699e7f
rj-here <r.rishi.rj@gmail.com>
2025-07-30 12:26:49 -0400

    Corrections, tweaks!

--- PlayingXICricket.py
+++ PlayingXICricket.py
@@ -8,7 +8,7 @@ while (len(my_playing_XI) < 11):
     my_playing_XI.update({player_name: ''}) #Add the player to the dictionary with an empty role.
 for player in my_playing_XI:
     #Now, we will get the role(s) of each player
-    role = input(f'Enter the role of {player} (batter, bowler, allrounder, wicketkeeper): ')
+    role = input(f'Enter the role of {player} (batter, bowler, allrounder (including batting and bowling allrounders), wicketkeeper): ')
     role = role.lower()
     #To check if the role is valid
     valid_roles = ['batter', 'Batter', 'BATTER', 'bowler', 'Bowler', 'BOWLER', 'allrounder', 'Allrounder', 'ALLROUNDER', 'bowling allrounder', 'Bowling Allrounder', 'BOWLING ALLROUNDER', 'batting allrounder', 'Batting Allrounder', 'BATTING ALLROUNDER', 'wicketkeeper', 'Wicketkeeper', 'WICKETKEEPER']
@@ -23,21 +23,18 @@ for player in my_playing_XI:
 
 #Now, to define the captain, vice-captain
 captain = input('Enter the name of the captain: ') #For the captain
-if captain not in my_playing_XI:
+while captain not in my_playing_XI:
     print(f'{captain} is not in the playing XI! Please enter a valid captain.')
     captain = input('Enter the name of the captain: ')
-else:
-    print(f'{captain} is set as the captain of the playing XI.')
+
+print(f'{captain} is set as the captain of the playing XI.')
 
 viceCaptain = input('Enter the name of the vice-captain: ') #For the vice-captain
-if viceCaptain not in my_playing_XI:
-    print(f'{viceCaptain} is not in the playing XI! Please enter a valid vice-captain.')
-    viceCaptain = input('Enter the name of the vice-captain: ')
-elif viceCaptain == captain:
-    print(f'{viceCaptain} is already the captain! Enter their name of their deupty!')
-    viceCaptain = input('Enter the name of the vice-captain: ')
-else:
-    print(f'{viceCaptain} is set as the vice-captain of the playing XI.')
+while viceCaptain not in my_playing_XI or viceCaptain == captain:
+    print(f'{viceCaptain} is not a valid vice-captain, please enter a valid one!')
+    viceCaptain = input(f'Enter a valid vice-captain: ')
+    
+print(f'{viceCaptain} is set as the vice-captain of the playing XI.')
 
 print('Your playing XI is ready!')
 for player, role in my_playing_XI.items():
@@ -47,8 +44,6 @@ for player, role in my_playing_XI.items():
         print(f'{player} ({role}, vc)')
     else:
         print(f'{player} ({role})')
-    
-
 
 #© 2025 Rishi
-#Last updated: 23rd July, 2025
+#Last updated: 30th July, 2025

cadee67866b306cda66864e38ffc0b455aacbc9a
rj-here <r.rishi.rj@gmail.com>
2025-08-20 19:55:38 -0400

    Just adding some fun messages.

--- TemperatureConvertor.py
+++ TemperatureConvertor.py
@@ -1,5 +1,6 @@
 #This program will help you convert temperature from degrees Celsius to Fahrenheit and vice versa.
 #Source: https://www.calculatorsoup.com/calculators/conversions/celsius-to-fahrenheit.php
+import random
 print("Welcome to the Temperature Converter!\nHere, you'll convert temperature from ºC to ºF and vice versa")
 print("Let's get started!")
 yesOrNo = "yes"
@@ -15,5 +16,9 @@ while (yesOrNo.lower() == "yes"):
             print(f"{fahrenheit}ºF = {celsius}ºC")
     yesOrNo = input("Would you like to do another conversion? (yes/no): ")
 print("Thank you for using the Temperature Converter!")
+#A variation of messages, just for fun!
+messages = ["Temperatures fluctuate, but so does life at times...", "The weather might be hot, but you should stay cool!", "Keep calm and convert on!"]
+number = random.randint(0, 2)
+print(messages[number])
 #© 2025 Rishi
 #Last updated: 13th August, 2025
